8c8aed355869802f3cd2cf56fa79a3fe82976495
Trevor Vannoy <trevorvannoy@montana.edu>
2023-02-06 17:23:42 -0700

    add features and extra labels to training/testing data

--- orientationTrainTestSplit.m
+++ orientationTrainTestSplit.m
@@ -5,9 +5,9 @@ clear
 % Set random number generator properties for reproducibility
 rng(0, 'twister');
 
-rawDataDir = '../data/raw';
+rawDataDir = '../data/preprocessed';
 newDataDir = '../data';
-datafile = 'combined-data-2022-12-16.mat';
+datafile = 'preprocessedScans.mat';
 
 %% Folder indices for the on-axis and perpendicular orientation data
 onAxisOrientationIdx = [1,2,3,4,5,6,7,8,21,22,23,24];
@@ -18,20 +18,37 @@ load([rawDataDir filesep datafile])
 
 %% Restructure the data structures to be more conducive
 labels = {data.Labels}';
+speedLabels = {data.SpeedLabels}';
+positionLabels = {data.PositionLabels}';
+orientationLabels = {data.OrientationLabels}';
 dataCellArray = {data.Data}';
+featuresCellArray = {data.Features}';
 
 
 onAxisData = dataCellArray(onAxisOrientationIdx);
 perpendicularData = dataCellArray(perpendicularOrientationIdx);
+
+onAxisFeatures = featuresCellArray(onAxisOrientationIdx);
+perpendicularFeatures = featuresCellArray(perpendicularOrientationIdx);
+
 onAxisLabels = labels(onAxisOrientationIdx);
 perpendicularLabels = labels(perpendicularOrientationIdx);
-
+onAxisSpeedLabels = speedLabels(onAxisOrientationIdx);
+perpendicularSpeedLabels = speedLabels(perpendicularOrientationIdx);
+onAxisPositionLabels = positionLabels(onAxisOrientationIdx);
+perpendicularPositionLabels = positionLabels(perpendicularOrientationIdx);
+onAxisOrientationLabels = orientationLabels(onAxisOrientationIdx);
+perpendicularOrientationLabels = orientationLabels(perpendicularOrientationIdx);
 
 %% Save training and testing data
 % mkdir(newDataDir, 'testing');
 save([newDataDir filesep 'on-axis' filesep 'onAxisData.mat'], ...
-    'onAxisData', 'onAxisLabels', '-v7.3');
+    'onAxisData', 'onAxisFeatures', 'onAxisLabels', ...
+    'onAxisOrientationLabels', 'onAxisPositionLabels', ...
+    'onAxisSpeedLabels', '-v7.3');
 
 % mkdir(newDataDir, 'training');
 save([newDataDir filesep 'perpendicular' filesep 'perpendicularData.mat'], ...
-    'perpendicularData', 'perpendicularLabels', '-v7.3');
+    'perpendicularData', 'perpendicularFeatures', 'perpendicularLabels', ...
+    'perpendicularOrientationLabels', 'perpendicularPositionLabels', ...
+    'perpendicularSpeedLabels', '-v7.3');
--- randomTrainTestSplit.m
+++ randomTrainTestSplit.m
@@ -5,16 +5,20 @@ clear
 % Set random number generator properties for reproducibility
 rng(0, 'twister');
 
-rawDataDir = '../data/raw';
+rawDataDir = '../data/preprocessed';
 newDataDir = '../data';
-datafile = 'combined-data-2022-12-16.mat';
+datafile = 'preprocessedScans.mat';
 
 %% Load data
 load([rawDataDir filesep datafile])
 
 %% Restructure the data structures to be more conducive
 labels = {data.Labels}';
+speedLabels = {data.SpeedLabels}';
+positionLabels = {data.PositionLabels}';
+orientationLabels = {data.OrientationLabels}';
 dataCellArray = {data.Data}';
+featuresCellArray = {data.Features}';
 
 
 %% Partition into training and test sets
@@ -24,17 +28,29 @@ holdoutPartition = cvpartition(length(dataCellArray), 'Holdout', TEST_PCT);
 
 trainingData = dataCellArray(training(holdoutPartition));
 testingData = dataCellArray(test(holdoutPartition));
+
+trainingFeatures = featuresCellArray(training(holdoutPartition));
+testingFeatures = featuresCellArray(test(holdoutPartition));
+
 trainingLabels = labels(training(holdoutPartition));
 testingLabels = labels(test(holdoutPartition));
+trainingSpeedLabels = speedLabels(training(holdoutPartition));
+testingSpeedLabels = speedLabels(test(holdoutPartition));
+trainingPositionLabels = positionLabels(training(holdoutPartition));
+testingPositionLabels = positionLabels(test(holdoutPartition));
+trainingOrientationLabels = orientationLabels(training(holdoutPartition));
+testingOrientatinoLabels = orientationLabels(test(holdoutPartition));
 
 
 %% Save training and testing data
 % mkdir(newDataDir, 'testing');
 save([newDataDir filesep 'testing' filesep 'testingDataRandomSplit.mat'], ...
-    'testingData', 'testingLabels', ...
-    'holdoutPartition', '-v7.3');
+    'testingData', 'testingLabels', 'testingSpeedLabels', ...
+    'testingOrientatinoLabels', 'testingPositionLabels', ...
+    'testingFeatures', 'holdoutPartition', '-v7.3');
 
 % mkdir(newDataDir, 'training');
 save([newDataDir filesep 'training' filesep 'trainingDataRandomSplit.mat'], ...
-    'trainingData', 'trainingLabels', ...
-    'holdoutPartition', '-v7.3');
+    'trainingData', 'trainingLabels', 'trainingSpeedLabels', ...
+    'trainingOrientationLabels', 'trainingPositionLabels', ...
+    'trainingFeatures', 'holdoutPartition', '-v7.3');
