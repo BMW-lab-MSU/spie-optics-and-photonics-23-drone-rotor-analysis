8e444c8b5e42e2f1d4c384bb0c5e17696bc993f4
Trevor Vannoy <trevorvannoy@montana.edu>
2023-02-08 11:05:16 -0700

    add on-axis experiments

---
+++ experimentTrainOnAxisSpeed.m
@@ -0,0 +1,15 @@
+% SPDX-License-Identifier: BSD-3-Clause
+%% Load data
+clear
+
+datadir = '../data/on-axis';
+datafile = 'onAxisData.mat';
+
+load([datadir filesep datafile])
+
+%% prepare data
+features = nestedcell2mat(onAxisFeatures);
+labels = nestedcell2mat(onAxisSpeedLabels);
+
+%% Run the classification learner
+classificationLearner(features, labels);
