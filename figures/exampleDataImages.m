b26728f7e5b604e6f64e889cb2bba359b254309b
Trevor Vannoy <trevorvannoy@montana.edu>
2023-02-16 22:02:58 -0700

    update example image figure

--- figures/exampleDataImages.m
+++ figures/exampleDataImages.m
@@ -18,7 +18,7 @@ load([onAxisPropPath filesep filename]);
 onAxisProp = adjusted_data_junecal(1);
 
 load([perpendicularPropPath filesep filename]);
-perpendciularProp = adjusted_data_junecal(1);
+perpendicularProp = adjusted_data_junecal(1);
 
 load([perpendicularMotorBodyPath filesep filename]);
 perpendicularMotorBody = adjusted_data_junecal(1);
@@ -31,11 +31,14 @@ close all
 fig = figure('Units','inches','Position',[2 2 8 3.5]);
 tiledChartObj = tiledlayout(fig, 2, 2);
 
+tiledChartObj.Padding = 'loose';
+tiledChartObj.TileSpacing = 'compact';
+
 SEC_TO_MS = 1e3;
 
 rangeResolution = 0.75; %[m]
 motorRange = 2.7; %[m]
-motorRangeBin = 3; %[m]
+motorRangeBin = 3;
 rangeStart = motorRange - rangeResolution * (motorRangeBin - 1);
 rangeEnd = rangeStart + rangeResolution * (height(onAxisOverlap.raw_data) - 1);
 range = rangeStart:rangeResolution:rangeEnd;
@@ -45,37 +48,38 @@ cmap = colormap();
 colormap(flipud(cmap));
 
 nexttile
-imagesc(onAxisOverlap.time * SEC_TO_MS, range(1:30), onAxisOverlap.raw_data(1:30,:));
+imagesc(onAxisOverlap.time * SEC_TO_MS, range(1:30), onAxisOverlap.data(1:30,:));
 xlim([0 150])
 set(gca, 'FontSize', 10, 'FontName', 'Times New Roman')
-title('(a)','Position',[-5.5 3], 'FontSize', 11)
+title('(a)', 'Units', 'normalized', 'Position',[0 1.03 0], 'FontSize', 11)
+xticks([])
 
 
 nexttile
-imagesc(onAxisProp.time * SEC_TO_MS, range(1:30), onAxisProp.raw_data(1:30,:));
+imagesc(onAxisProp.time * SEC_TO_MS, range(1:30), onAxisProp.data(1:30,:));
 xlim([0 150])
 set(gca, 'FontSize', 10, 'FontName', 'Times New Roman')
-title('(b)','Position',[-6 3], 'FontSize', 11)
+title('(b)', 'Units', 'normalized', 'Position',[0 1.03 0], 'FontSize', 11)
+xticks([])
 
 
 nexttile
-imagesc(perpendciularProp.time * SEC_TO_MS, range(1:30), perpendciularProp.raw_data(1:30,:));
+imagesc(perpendicularProp.time * SEC_TO_MS, range(1:30), perpendicularProp.data(1:30,:));
 xlim([0 150])
 set(gca, 'FontSize', 10, 'FontName', 'Times New Roman')
-title('(c)','Position',[-5.5 3], 'FontSize', 11)
+title('(c)', 'Units', 'normalized', 'Position',[0 1.03 0], 'FontSize', 11)
 
 
 nexttile
-imagesc(perpendicularMotorBody.time * SEC_TO_MS, range(1:30), perpendicularMotorBody.raw_data(1:30,:));
+imagesc(perpendicularMotorBody.time * SEC_TO_MS, range(1:30), perpendicularMotorBody.data(1:30,:));
 xlim([0 150])
 set(gca, 'FontSize', 10, 'FontName', 'Times New Roman')
-title('(d)','Position',[-6 3], 'FontSize', 11)
-
+title('(d)', 'Units', 'normalized', 'Position',[0 1.03 0], 'FontSize', 11)
 
 cbar = colorbar;
 cbar.Layout.Tile = 'east';
 cbar.YDir = 'reverse';
-cbar.Label.String = 'PMT current (arb.)';
+cbar.Label.String = 'PMT voltage (V)';
 cbar.Label.FontSize = 11;
 cbar.Label.FontName = 'Times New Roman';
 cbar.FontSize = 10;
@@ -83,8 +87,7 @@ cbar.FontName = 'Times New Roman';
 
 tiledChartObj.XLabel.String = 'time (ms)';
 tiledChartObj.YLabel.String = 'distance (m)';
-tiledChartObj.Padding = 'loose';
-tiledChartObj.TileSpacing = 'compact';
+
 tiledChartObj.XLabel.FontName = 'Times New Roman';
 tiledChartObj.XLabel.FontSize = 11;
 tiledChartObj.YLabel.FontName = 'Times New Roman';
